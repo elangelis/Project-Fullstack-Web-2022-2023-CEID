eecf900db84f48e800bbd2af8915621c7bec7cbc
elangelis <73367290+elangelis@users.noreply.github.com>
2023-09-27 15:01:33 +0300

    main

--- 1.LoginPage/LoginMenuPage.php
+++ 1.LoginPage/LoginMenuPage.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="LoginPage.css">
   </head>
-  
   <body>  
     <div class="form">
       <div>
